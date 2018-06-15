171f1544d9723cfb0fcaa7d9d94687aee2a7cf14
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2018-06-15 22:50:37 +0530

    player added

--- src/List.jsx
+++ src/List.jsx
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import './List.css'
-import {Link} from 'react-router'
-import Video from './Video'
+import VideoPlay from './VideoPlay'
 
 class List extends Component {
 
 		constructor(props){
 			super(props);
 			this.state = {
-				clickedVideoId: null
+				clickedId: null,
+				type: null
 			}
 		}
 
@@ -16,54 +16,82 @@ class List extends Component {
 
 		let results = this.props.data
 
-
-
-		return (
-
-			<div className = "results">
-			{results.map((result, k) => {
-				let type = result.kind.split('#')[1]
+		{
+			if(this.state.clickedId !== null )
 				return(
-					<div
-						key = {k}
-						className = "result"
-					>
-					{
-						type === 'channel' ? 
-							<div className="channel-card">
-								<div>
-								<img
-									src = {result.thumbnails.medium.url}
-						            alt = "track"
-								/>
-								</div>
-								<div>
-								{result.title}<br></br>
-								{result.description}
-								</div>
-							</div>
-						:
-							<div className="video-card" onClick = {() => this.setState({clickedVideoId: result.id})}>
-								<div>
-								<img
-									src = {result.thumbnails.medium.url}
-						            alt = "track"
-								/>
-								</div>
-								<div>
-								{result.title}<br></br>
-								{result.description}
-								</div>
-							</div>
-					}
-
+					<div>
+					<div>Video</div>
+					<VideoPlay
+						id = {this.state.clickedId}
+						type = {this.state.type}
+					/>
 					</div>
 				)
-			})}
-			</div>
-	
-		)
-	
+			else
+
+		
+
+			return (
+
+				<div className = "results">
+					{results.map((result, k) => {
+						let type = result.kind.split('#')[1]
+						return(
+							<div
+								key = {k}
+								className = "result"
+							>
+							{
+								type === 'channel' ? 
+									<div className="channel-card">
+										<div>
+										<img
+											src = {result.thumbnails.medium.url}
+								            alt = "track"
+										/>
+										</div>
+										<div>
+										{result.title}<br></br>
+										{result.description}
+										</div>
+									</div>
+								:
+									type === 'video' ?
+										<div className="video-card" onClick = {() => this.setState({clickedId: result.id, type: 'video'})}>
+											<div>
+											<img
+												src = {result.thumbnails.medium.url}
+									            alt = "track"
+											/>
+											</div>
+											<div>
+											{result.title}<br></br>
+											{result.description}
+											</div>
+										</div>
+									:
+										<div className="playlist-card" onClick = {() => this.setState({clickedId: result.id, type: 'playlist'})}>
+											<div>
+											<img
+												src = {result.thumbnails.medium.url}
+									            alt = "track"
+											/>
+											</div>
+											<div>
+											{result.title}<br></br>
+											{result.description}
+											</div>
+										</div>
+							}
+
+							</div>
+						)
+					})}
+				</div>
+		
+			)
+		}
+		
 	}
 }
 
---
+++ src/VideoPlay.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class VideoPlay extends Component {
+	constructor(props){
+		super(props);
+	}
+	render() {
+		var id = this.props.id
+		var URL
+		if(this.props.type === 'video')
+			URL = `https://www.youtube.com/embed/${id}?autoplay=1&loop=1`
+		else
+			URL = `http://www.youtube.com/embed?listType=playlist&list=${id}&autoplay=1&loop=1`
+		return(
+			<iframe width="1280" height="720" src={URL} frameborder="0" allowfullscreen></iframe>
+		)
+	}
+}
+
+export default VideoPlay
+
+
